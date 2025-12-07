6e82b4f4c778c2f5b84297f7c51438a762d01794
Nassim-chaieb <chaiebnassim14@gmail.com>
2025-12-08 00:23:17 +0100

    Update figurine.js

--- figurine.js
+++ figurine.js
@@ -1,15 +1,18 @@
-const images = document.querySelectorAll(".gallery img");
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
+document.addEventListener("DOMContentLoaded", () => {
 
-images.forEach(img => {
-    img.addEventListener("click", () => {
-        lightboxImg.src = img.src;
-        lightbox.style.display = "flex";
+    const images = document.querySelectorAll(".gallery img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    images.forEach(img => {
+        img.addEventListener("click", () => {
+            lightboxImg.src = img.src;
+            lightbox.style.display = "flex";
+        });
     });
-});
 
-// Fermer la lightbox quand on clique dessus
-lightbox.addEventListener("click", () => {
-    lightbox.style.display = "none";
+    lightbox.addEventListener("click", () => {
+        lightbox.style.display = "none";
+    });
+
 });
