969a05689304f26739840c86a1af9f53ab0d55d5
Nassim-chaieb <chaiebnassim14@gmail.com>
2025-12-07 02:51:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 // Carousel auto défilement (version améliorée)
 // défilement ultra fluide et continu
+document.addEventListener("DOMContentLoaded", () => {
 
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.photo-section');
-    if(!container) return;
+    const track = document.querySelector(".track");
 
-    let speed = 1; // px par frame
+    if(!track) return;
+
+    let speed = 1;
 
     function animate(){
-        container.scrollLeft += speed;
+        track.scrollLeft += speed;
 
-        if(container.scrollLeft >= container.scrollWidth - container.clientWidth){
-            container.scrollLeft = 0;
+        if(track.scrollLeft >= track.scrollWidth/2){
+            track.scrollLeft = 0;
         }
 
         requestAnimationFrame(animate);
     }
 
     animate();
+
 });
+
 /* Effet glass + ombre pour catégories */
 .categorie-btn{
     background: rgba(255,255,255,0.15);
